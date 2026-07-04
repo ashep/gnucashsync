2c1c1a4320508ababa4a661cdd2a2f5e9b255980
Oleksandr Shepetko <a@shepetko.com>
2026-07-04 16:32:25 +0300

    feat: GnuCash namespace constants and XML decode structs

---
+++ internal/gnucash/namespaces.go
@@ -0,0 +1,47 @@
+package gnucash
+
+const (
+	nsGnc   = "http://www.gnucash.org/XML/gnc"
+	nsAct   = "http://www.gnucash.org/XML/act"
+	nsBook  = "http://www.gnucash.org/XML/book"
+	nsTrn   = "http://www.gnucash.org/XML/trn"
+	nsSplit = "http://www.gnucash.org/XML/split"
+	nsTs    = "http://www.gnucash.org/XML/ts"
+	nsCmdty = "http://www.gnucash.org/XML/cmdty"
+	nsSlot  = "http://www.gnucash.org/XML/slot"
+)
+
+// rawAccount is decoded from <gnc:account> elements during the scan pass.
+type rawAccount struct {
+	Name   string `xml:"http://www.gnucash.org/XML/act name"`
+	ID     struct {
+		Value string `xml:",chardata"`
+	} `xml:"http://www.gnucash.org/XML/act id"`
+	Parent *struct {
+		Value string `xml:",chardata"`
+	} `xml:"http://www.gnucash.org/XML/act parent"`
+}
+
+func (a rawAccount) parentGUID() string {
+	if a.Parent == nil {
+		return ""
+	}
+	return a.Parent.Value
+}
+
+// rawSlot is a single key/value slot.
+type rawSlot struct {
+	Key   string `xml:"http://www.gnucash.org/XML/slot key"`
+	Value string `xml:"http://www.gnucash.org/XML/slot value"`
+}
+
+// rawSlots is the <trn:slots> container.
+type rawSlots struct {
+	Slots []rawSlot `xml:"http://www.gnucash.org/XML/slot slot"`
+}
+
+// rawTrn extracts only the slots from a <gnc:transaction> — we don't need
+// the full transaction for the scan pass.
+type rawTrn struct {
+	Slots rawSlots `xml:"http://www.gnucash.org/XML/trn slots"`
+}
